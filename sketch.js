653fda26cd529a0c94b312f548c073b23fcc25ea
suki-suki <sukrit96singh@gmail.com>
2016-03-28 00:07:58 -0400

    done!

--- sketch.js
+++ sketch.js
@@ -1,45 +1,35 @@
-//================================
-//	view your project using anvil at:
-//	http://images.dev
-//
-// or after you upload it to github at:
-//	http://yourusername.github.io/images/
-//================================
 
-//STEP 1: Declare global variables for your images and upload button
+
+var photo;
+var uploadButton;
+var uploadedImage;
 
 function preload(){
-	//STEP 2:
-	// Load your image with loadImage("path/to/image.jpg")
-	//and save it to your global variable:
 
+	photo= loadImage("../photos/IMG_5669.jpg");
 
 }
 
 function setup() {
 
-	//STEP 3
-	//add a file upload button with CreateFileInput(_____);
+//CreateFileInput();
+var uploadButton = createFileInput(imageUploaded)
 
-	//STEP 4:
-  //set up your canvas with createCanvas(__,__);
-
+createCanvas(1400,1200);
+image (photo,0,0);
 }
 
 function draw() {
 
-	//STEP 5:
-	//draw your images with image(imagevariable);
-
-	//STEP 7:
-	//You're also going to need an if statement here to check
-	//if the image the user uploaded exists to then draw it
-	//(see image upload example version 2)
+// image (photo,0,0);
 
 }
 
-//STEP 6:
-//create a callback function (you can name it anything you'd like)
-//which will be called when the user uploads an image:
+function imageUploaded(file){
+  uploadedImage = loadImage(file.data, drawImg);
+}
 
-	//and inside it you will need to save the loaded image to a global variable
+function drawImg(){
+
+  image(uploadedImage,0,0);
+}
